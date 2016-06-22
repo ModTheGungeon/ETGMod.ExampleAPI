bfb1382fadfd45b3cf8c3a87dc5e81ac6750d178
Maik Macho <nuda1998@gmail.com>
2016-06-22 23:48:35 +0200

    Let's just hope this works

---
+++ Assembly-CSharp.ExampleAPI.mm/src/SpeedrunTimer.cs
@@ -0,0 +1,27 @@
+﻿// OPTIONAL: Disabling some warnings
+// Add this in every class using extern orig_ methods to tell the compiler to ignore the warnings.
+#pragma warning disable 0626
+// Add this in every class reimplementing fields that are never assigned to.
+#pragma warning disable 0649
+// END OPTIONAL
+
+// Feel free to use what you're using - you're not limited to the usings of the injecting type!
+using System;
+using System.Collections.Generic;
+
+// Gungeon / many Unity games don't use a namespace.
+public class patch_SpeedrunTimer : SpeedrunTimer {
+
+    private dfLabel dfLabel_0; // This field is private in SpeedrunTimer. It's the label.
+
+    private extern void orig_Update(); // The original method will end up here.
+    private void Update() {
+        // Setup stuff happens in the original method.
+        // Instead of copying it over, we just call it...
+        orig_Update();
+
+        // ... and replace the text.
+        dfLabel_0.Text = "NEW WR! ALWAYS!";
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Enter the Gungeon Example API Mod
+### License: MIT
+
+----
+
+This repository is a commented example API mod ("backend") for the game "Enter the Gungeon."
+
+It replaces the speedrun mode timer with a custom text.
+
+To see how to package an EtG mod, take a look into ETGMOD.zip
+
+For an example CI setup (Travis CI), take a look at build.sh, .travis.yml and [the Travis CI output](https://travis-ci.org/ModTheGungeon/ETGMod.ExampleMod).
